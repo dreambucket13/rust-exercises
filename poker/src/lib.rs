568e533ccd0f7d5a514039740ff3570166a48f16
dreambucket13 <carmichr@gmail.com>
2023-02-01 21:10:15 -0500

    4 of a kind

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -17,7 +17,7 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
             Hand {
                 hand_string: raw_hand,
                 cards: Vec::new(),
-                value_count: vec!(0; 13),
+                value_count: vec!(0; 14),
                 suit_count: vec!(0; 4),
                 primary_rank: PrimaryRanks::NoRank,
                 secondary_rank: Vec::new()
@@ -36,15 +36,20 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
                 };
 
             hand.cards.push(card);
-
-            //hand.value_count[(value as i32)] += 1;
+            
+            //count the cards
+            hand.value_count[(value as usize)] += 1;
 
         }
+
+        println!("{:?}", hand.value_count);
 
         //sort cards by value
 
         println!("{:?}",hand.cards);
         hand.cards.sort_unstable_by_key(|card| card.value);
+        //we want the highest value cards first
+        hand.cards.reverse();
         println!("{:?}",hand.cards);
 
         parsed_hands.push(hand);
@@ -53,9 +58,13 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
 
     //assign ranks
 
-    println!("{:?}",parsed_hands[0]);
-    assign_rank(&mut parsed_hands[0]);
-    println!("{:?}",parsed_hands[0]);
+
+
+    for hand_to_score in &mut parsed_hands {
+        assign_rank(hand_to_score);
+    }
+
+
     
     //push highest ranks to best hands
 
@@ -98,8 +107,11 @@ fn detect_straight_flush(hand_to_score: &mut Hand) -> bool{
 
     if detect_flush(hand_to_score) && detect_straight(hand_to_score){
         hand_to_score.primary_rank = PrimaryRanks::StraightFlush;
+
+        //have to override secondary rank, flushes are ranked by all cards
+        hand_to_score.secondary_rank.clear();
         //push high card value to secondary rank (hand is already sorted)
-        hand_to_score.secondary_rank.push(hand_to_score.cards[4].value);
+        hand_to_score.secondary_rank.push(hand_to_score.cards[0].value);
         return true;
     }
 
@@ -108,11 +120,24 @@ fn detect_straight_flush(hand_to_score: &mut Hand) -> bool{
 }
 
 fn detect_four_of_a_kind(hand_to_score: &mut Hand) -> bool {
+
+    if hand_to_score.value_count.contains(&4){
+
+        hand_to_score.primary_rank = PrimaryRanks::FourOfAKind;
+
+        let index = hand_to_score.value_count.iter().position(|&x| x == 1).unwrap();
+
+        hand_to_score.secondary_rank.push(usize_to_card_value(index));
+
+        return true;
+    }
     
     false
 }
 
 fn detect_full_house(hand_to_score: &mut Hand) -> bool {
+
+
 
     false
     
@@ -132,14 +157,32 @@ fn detect_flush(hand_to_score: &mut Hand) -> bool {
     }
 
     hand_to_score.primary_rank = PrimaryRanks::Flush;
+
+    for card in &hand_to_score.cards{
+        hand_to_score.secondary_rank.push(card.value);
+    }
+
     return true;
 
 }
 
 fn detect_straight(hand_to_score: &mut Hand) -> bool {
 
-    let mut prior_card = &hand_to_score.cards[0];
+    //special case for ace low straight, simply check for 1 each of A,2,3,4,5
+    //kicker is 5 not ace though
+    if hand_to_score.value_count[CardValues::AceHigh as usize] == 1 &&
+       hand_to_score.value_count[CardValues::TWO as usize] == 1 &&
+       hand_to_score.value_count[CardValues::THREE as usize] == 1 &&
+       hand_to_score.value_count[CardValues::FOUR as usize] == 1 &&
+       hand_to_score.value_count[CardValues::FIVE as usize] == 1
+    {
+        hand_to_score.primary_rank = PrimaryRanks::Straight;
+        hand_to_score.secondary_rank.push(CardValues::FIVE);
+        return true;
+    }
 
+    //probably can refactor this to look for 5 1 counts in a row in the value_count array
+    let mut prior_card = &hand_to_score.cards[0];
 
     for card in &hand_to_score.cards[1..]{
 
@@ -151,13 +194,15 @@ fn detect_straight(hand_to_score: &mut Hand) -> bool {
 
     }
 
-    //need special case for ace low straight flush?  simply check for 1 each of A,2,3,4,5
-    
     hand_to_score.primary_rank = PrimaryRanks::Straight;
+    //kicker is the max value, which is the first element
+    hand_to_score.secondary_rank.push(hand_to_score.cards[0].value);
     return true;
 }
 
 fn detect_three_of_a_kind(hand_to_score: &mut Hand) -> bool{
+
+
 
     false
     
@@ -175,33 +220,58 @@ fn detect_one_pair(hand_to_score: &mut Hand) -> bool{
 
 fn detect_high_card(hand_to_score: &mut Hand) -> bool{
     
+    hand_to_score.primary_rank = PrimaryRanks::HighCard;
+
+    for card in &hand_to_score.cards{
+        hand_to_score.secondary_rank.push(card.value);
+    }
+
     true
 }
 
 
 fn str_to_card_value(str: &str) -> CardValues {
 
-    let value: CardValues;
-
     match str {
 
-        _ if str.starts_with("2") => value = CardValues::TWO,
-        _ if str.starts_with("3") => value = CardValues::THREE,
-        _ if str.starts_with("4") => value = CardValues::FOUR,
-        _ if str.starts_with("5") => value = CardValues::FIVE,
-        _ if str.starts_with("6") => value = CardValues::SIX,
-        _ if str.starts_with("7") => value = CardValues::SEVEN,
-        _ if str.starts_with("8") => value = CardValues::EIGHT,
-        _ if str.starts_with("9") => value = CardValues::NINE,
-        _ if str.starts_with("J") => value = CardValues::JACK,
-        _ if str.starts_with("Q") => value = CardValues::QUEEN,
-        _ if str.starts_with("K") => value = CardValues::KING,
-        _ if str.starts_with("A") => value = CardValues::AceHigh,
-        _ => value = CardValues::NoValue,
+        _ if str.starts_with("2") => return CardValues::TWO,
+        _ if str.starts_with("3") => return CardValues::THREE,
+        _ if str.starts_with("4") => return CardValues::FOUR,
+        _ if str.starts_with("5") => return CardValues::FIVE,
+        _ if str.starts_with("6") => return CardValues::SIX,
+        _ if str.starts_with("7") => return CardValues::SEVEN,
+        _ if str.starts_with("8") => return CardValues::EIGHT,
+        _ if str.starts_with("9") => return CardValues::NINE,
+        _ if str.starts_with("J") => return CardValues::JACK,
+        _ if str.starts_with("Q") => return CardValues::QUEEN,
+        _ if str.starts_with("K") => return CardValues::KING,
+        _ if str.starts_with("A") => return CardValues::AceHigh,
+        _ => return CardValues::NoValue,
 
     };
 
-    value
+}
+
+fn usize_to_card_value(index: usize) -> CardValues {
+
+    match index {
+
+        1 => return CardValues::AceLow,
+        2 => return CardValues::TWO,
+        3 => return CardValues::THREE,
+        4 => return CardValues::FOUR,
+        5 => return CardValues::FIVE,
+        6 => return CardValues::SIX,
+        7 => return CardValues::SEVEN,
+        8 => return CardValues::EIGHT,
+        9 => return CardValues::NINE,
+        10 => return CardValues::JACK,
+        11 => return CardValues::QUEEN,
+        12 => return CardValues::KING,
+        13 => return CardValues::AceHigh,
+        _ => return CardValues::NoValue,
+
+    };
 
 }
 
